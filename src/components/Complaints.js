14480f9c8cab37375328ede51183709ff02f4dbe
TanveerSodhi <60098315+TanveerSodhi@users.noreply.github.com>
2020-06-30 17:14:15 +0530

    Actions

--- src/components/Complaints.js
+++ src/components/Complaints.js
@@ -1,47 +1,8 @@
 import React from 'react';
 import { MDBDataTableV5} from 'mdbreact';
 
-export default function Complaints() {
-  const [datatable, setDatatable] = React.useState({
-    columns: [
-      {
-        label: 'Student Name',
-        field: 'name',
-        width: 150,
-        attributes: {
-          'aria-controls': 'DataTable',
-          'aria-label': 'Name',
-        },
-      },
-      {
-        label: 'Room No',
-        field: 'room',
-        width: 150,
-      },
-      {
-        label: 'Complaint',
-        field: 'complaint',
-        width: 150,
-      },
-      
-      {
-        label: 'Actions',
-        field: 'actions',
-        
-        width: 100,
-      },
-
-    ],
-    rows: [
-        {
-        name: 'Jatin Bansal',
-        room: '2021',
-        complaint: 'darwaaza sahi kraayo mera',
-        actions: 'Buttons'
-    },
+export default function Complaints({complaints}) {
   
-],
-});
 
 return (
 <div>
@@ -58,7 +19,7 @@ return (
   entriesOptions={[5, 20, 25]}
   entries={5}
   pagesAmount={4}
-  data={datatable}
+  data={complaints}
   pagingTop
   searchTop
   searchBottom={false}
