8e7bb3ce671401d275a84f494d362b9f52f06fb6
bansaljatin05 <58761492+bansaljatin05@users.noreply.github.com>
2020-08-21 22:41:02 +0530

    actionCreators

---
+++ src/redux/actionCreators.js
@@ -0,0 +1,195 @@
+import * as ActionTypes from './ActionTypes';
+import { baseUrl } from '../shared/baseUrl';
+
+export const studentsLoading = () => ({
+    type: ActionTypes.STUDENTS_LOADING
+});
+
+export const studentsFailed = (errmess) => ({
+    type: ActionTypes.STUDENTS_FAILED,
+    payload: errmess
+});
+
+export const studentsSuccess = (students) => ({
+    type: ActionTypes.STUDENTS_SUCCESS,
+    payload: students
+});
+
+export const addStudent = (student) => ({
+    type: ActionTypes.ADD_STUDENT,
+    payload: student
+}); 
+
+export const postStudent = (student) => (dispatch) => {
+
+    const newStudent = {
+        studentName: student.name,
+        sid: student.id,
+        mobileNo: student.mobile,
+        gender: student.gender,
+        email: student.email,
+        branch: student.branch,
+        nationality: student.nationality,
+        address: student.address,
+        fatherName: student.father,
+        motherName: student.mother,
+        fatherMobile: student.Fnum,
+        dob: student.dob,
+        photo: student.photo
+    }
+    console.log('Student: ', newStudent);
+
+    const bearer = 'Bearer ' + localStorage.getItem('token');
+
+    return fetch(baseUrl + 'students', {
+        method: 'POST',
+        body: JSON.stringify(newStudent),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': bearer
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response;
+        }
+        else {
+            var error = new Error('Error ' + response.status + ': ' + response.statusText);
+            error.response = response;
+            throw error;
+        }
+    },
+    error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+    })
+    .then(response => response.json())
+    .then(response => dispatch(addStudent(response)))
+    .catch(error => { console.log('Post students ', error.message);
+        alert('Your student could not be added\nError: '+ error.message); })
+}
+
+export const fetchStudents = () => (dispatch) => {
+    dispatch(studentsLoading(true));
+
+    const bearer = 'Bearer ' + localStorage.getItem('token');
+
+    return fetch(baseUrl + 'students', {
+        headers: {
+            'method': 'GET',
+            'Authorization': bearer
+        },
+    })
+    .then(response => {
+        if (response.ok) {
+            return response;
+        }
+        else {
+            var error = new Error('Error ' + response.status + ': ' + response.statusText);
+            error.response = response;
+            throw error;
+        }
+    },
+    error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+    })
+    .then(response => response.json())
+    .then(students => dispatch(studentsSuccess(students)))
+    .catch(error => dispatch(studentsFailed(error.message)));
+}
+
+export const requestLogin = (creds) => {
+    return {
+        type: ActionTypes.LOGIN_REQUEST,
+        creds
+    }
+}
+  
+export const receiveLogin = (response) => {
+    return {
+        type: ActionTypes.LOGIN_SUCCESS,
+        token: response.token
+    }
+}
+  
+export const loginError = (message) => {
+    return {
+        type: ActionTypes.LOGIN_FAILURE,
+        message
+    }
+}
+
+export const loginUser = (creds) => (dispatch) => {
+    // We dispatch requestLogin to kickoff the call to the API
+    dispatch(requestLogin(creds))
+
+    return fetch(baseUrl + 'users/login', {
+        method: 'POST',
+        headers: { 
+            'Content-Type':'application/json' 
+        },
+        body: JSON.stringify(creds)
+    })
+    .then(response => {
+        if (response.ok) {
+            return response;
+        } else {
+            var error = new Error('Error ' + response.status + ': ' + response.statusText);
+            error.response = response;
+            throw error;
+        }
+        },
+        error => {
+            throw error;
+        })
+    .then(response => response.json())
+    .then(response => {
+        if (response.success) {
+            // If login was successful, set the token in local storage
+            localStorage.setItem('token', response.token);
+            localStorage.setItem('creds', JSON.stringify(creds));
+            // Dispatch the success action
+            dispatch(fetchStudents());
+            dispatch(fetchEmployees());
+            dispatch(fetchNotices());
+            dispatch(fetchMeals());
+            dispatch(fetchMealBills());
+            dispatch(fetchNotices());
+            dispatch(receiveLogin(response));
+        }
+        else {
+            var error = new Error('Error ' + response.status);
+            error.response = response;
+            throw error;
+        }
+    })
+    .catch(error => dispatch(loginError(error.message)))
+};
+
+export const requestLogout = () => {
+    return {
+      type: ActionTypes.LOGOUT_REQUEST
+    }
+}
+  
+export const receiveLogout = () => {
+    return {
+      type: ActionTypes.LOGOUT_SUCCESS
+    }
+}
+
+// Logs the user out
+export const logoutUser = () => (dispatch) => {
+    dispatch(requestLogout())
+    localStorage.removeItem('token');
+    localStorage.removeItem('creds');
+    dispatch(studentsFailed("Error 401: Unauthorized"));
+    dispatch(noticesFailed("Error 401: Unauthorized"));
+    dispatch(mealsFailed("Error 401: Unauthorized"));
+    dispatch(mealBillsFailed("Error 401: Unauthorized"));
+    dispatch(employeesFailed("Error 401: Unauthorized"));
+    dispatch(receiveLogout())
+}
+
+
---
+++ src/redux/auth.js
@@ -0,0 +1,45 @@
+import * as ActionTypes from './ActionTypes';
+
+export const Auth = (state = {
+        isLoading: false,
+        isAuthenticated: localStorage.getItem('token') ? true : false,
+        token: localStorage.getItem('token'),
+        user: localStorage.getItem('creds') ? JSON.parse(localStorage.getItem('creds')) : null,
+        errMess: null
+    }, action) => {
+    switch (action.type) {
+        case ActionTypes.LOGIN_REQUEST:
+            return {...state,
+                isLoading: true,
+                isAuthenticated: false,
+                user: action.creds
+            };
+        case ActionTypes.LOGIN_SUCCESS:
+            return {...state,
+                isLoading: false,
+                isAuthenticated: true,
+                errMess: '',
+                token: action.token
+            };
+        case ActionTypes.LOGIN_FAILURE:
+            return {...state,
+                isLoading: false,
+                isAuthenticated: false,
+                errMess: action.message
+            };
+        case ActionTypes.LOGOUT_REQUEST:
+            return {...state,
+                isLoading: true,
+                isAuthenticated: true
+            };
+        case ActionTypes.LOGOUT_SUCCESS:
+            return {...state,
+                isLoading: false,
+                isAuthenticated: false,
+                token: '',
+                user: null
+            };
+        default:
+            return state
+    }
+}
