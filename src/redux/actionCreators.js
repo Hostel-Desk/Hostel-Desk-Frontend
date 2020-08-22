94f2abc2e763e07ae69e3e2572f25094dfcafcd5
Puneet Bansal <puneetbansal15801@gmail.com>
2020-08-22 14:13:20 +0530

    redux notice and salary

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -99,6 +99,181 @@ export const fetchStudents = () => (dispatch) => {
     .catch(error => dispatch(studentsFailed(error.message)));
 }
 
+export const noticesLoading = () => ({
+    type: ActionTypes.NOTICES_LOADING
+});
+
+export const noticesFailed = (errmess) => ({
+    type: ActionTypes.NOTICES_FAILED,
+    payload: errmess
+});
+
+export const noticesSuccess = (notices) => ({
+    type: ActionTypes.NOTICES_SUCCESS,
+    payload: notices
+});
+
+export const addNotice = (notice) => ({
+    type: ActionTypes.ADD_NOTICE,
+    payload: notice
+}); 
+
+export const postNotice = (notice) => (dispatch) => {
+
+    const newNotice = {
+        title: notice.title,
+        description: notice.description
+    }
+    console.log('Notice: ', newNotice);
+
+    const bearer = 'Bearer ' + localStorage.getItem('token');
+
+    return fetch(baseUrl + 'notices', {
+        method: 'POST',
+        body: JSON.stringify(newNotice),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': bearer
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response;
+        }
+        else {
+            var error = new Error('Error ' + response.status + ': ' + response.statusText);
+            error.response = response;
+            throw error;
+        }
+    },
+    error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+    })
+    .then(response => response.json())
+    .then(response => dispatch(addNotice(response)))
+    .catch(error => { console.log('Post notices ', error.message);
+        alert('Your notice could not be added\nError: '+ error.message); })
+}
+
+export const fetchNotices = () => (dispatch) => {
+    dispatch(noticesLoading(true));
+
+    const bearer = 'Bearer ' + localStorage.getItem('token');
+
+    return fetch(baseUrl + 'notices', {
+        headers: {
+            'method': 'GET',
+            'Authorization': bearer
+        },
+    })
+    .then(response => {
+        if (response.ok) {
+            return response;
+        }
+        else {
+            var error = new Error('Error ' + response.status + ': ' + response.statusText);
+            error.response = response;
+            throw error;
+        }
+    },
+    error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+    })
+    .then(response => response.json())
+    .then(notices => dispatch(noticesSuccess(notices)))
+    .catch(error => dispatch(noticesFailed(error.message)));
+}
+
+export const salaryLoading = () => ({
+    type: ActionTypes.SALARY_LOADING
+});
+
+export const salaryFailed = (errmess) => ({
+    type: ActionTypes.SALARY_FAILED,
+    payload: errmess
+});
+
+export const salarySuccess = (salaries) => ({
+    type: ActionTypes.SALARY_SUCCESS,
+    payload: salaries
+});
+
+export const addSalary = (salary) => ({
+    type: ActionTypes.ADD_SALARY,
+    payload: salary
+}); 
+
+export const postSalary = (salary) => (dispatch) => {
+
+    const newSalary = {
+        name: salary.name,
+        month: salary.salaryMonth,
+        salary: salary.rupees
+    }
+    console.log('Salary: ', newSalary);
+
+    const bearer = 'Bearer ' + localStorage.getItem('token');
+
+    return fetch(baseUrl + 'salaries', {
+        method: 'POST',
+        body: JSON.stringify(newSalary),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': bearer
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response;
+        }
+        else {
+            var error = new Error('Error ' + response.status + ': ' + response.statusText);
+            error.response = response;
+            throw error;
+        }
+    },
+    error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+    })
+    .then(response => response.json())
+    .then(response => dispatch(addSalary(response)))
+    .catch(error => { console.log('Post salary', error.message);
+        alert('Your salary could not be added\nError: '+ error.message); })
+}
+
+export const fetchSalaries = () => (dispatch) => {
+    dispatch(salaryLoading(true));
+
+    const bearer = 'Bearer ' + localStorage.getItem('token');
+
+    return fetch(baseUrl + 'salaries', {
+        headers: {
+            'method': 'GET',
+            'Authorization': bearer
+        },
+    })
+    .then(response => {
+        if (response.ok) {
+            return response;
+        }
+        else {
+            var error = new Error('Error ' + response.status + ': ' + response.statusText);
+            error.response = response;
+            throw error;
+        }
+    },
+    error => {
+        var errmess = new Error(error.message);
+        throw errmess;
+    })
+    .then(response => response.json())
+    .then(salaries => dispatch(salarySuccess(salaries)))
+    .catch(error => dispatch(salaryFailed(error.message)));
+}
+
 export const requestLogin = (creds) => {
     return {
         type: ActionTypes.LOGIN_REQUEST,
