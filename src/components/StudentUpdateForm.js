561b865098972d5e0c4c63b6936df9d01d06c725
Aaryan Sharma <60098288+Aaryan8751@users.noreply.github.com>
2020-10-13 23:43:49 +0530

    class of student form update
    class of student form update

--- src/components/StudentUpdateForm.js
+++ src/components/StudentUpdateForm.js
@@ -1,66 +1,67 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 import { useParams } from 'react-router-dom';
 import {Form, Input, Button, Label, Col, Row, FormGroup, FormFeedback} from 'reactstrap';
 
-
-function StudentUpdateForm(props) {
-    const id = useParams();
-    //console.log(typeof(id.id));
-    const [initialState, setState] = useState({
-        id: id.id,
-        sid: '',
-        fullname: '',
-        mobile: '',
-        program: '',
-        father: '',
-        mother: '',
-        fnum: '',
-        address: '',
-        email: '',
-        touched: {
-            sid: false,
-            fullname: false,
-            mobile: false,
-            program: false,
-            father: false,
-            mother: false,
-            fnum: false,
-            address: false,
-            email: false
+class StudentUpdateForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sid: '',
+            fullname: '',
+            mobile: '',
+            program: '',
+            father: '',
+            mother: '',
+            fnum: '',
+            address: '',
+            email: '',
+            touched: {
+                sid: false,
+                fullname: false,
+                mobile: false,
+                program: false,
+                father: false,
+                mother: false,
+                fnum: false,
+                address: false,
+                email: false
+            }
         }
-    })
-
-    const handleBlur = (field) => (event) => {
-        setState({
-            ...initialState,
-            touched: {...initialState.touched, [field]: true}
-        })
     }
-    const handleInputChange = (event) => {
+
+    handleInputChange = (event) => {
         const target = event.target;
-        const value = event.value;
+        const value = target.value;
         const name = target.name;
 
-        setState({
-            ...initialState,
+        this.setState({
             [name]: value
         });
     }
 
-    const handleSubmit = (event) => {
+    handleSubmit = (event) => {
         event.preventDefault();
-        props.updateStudent(
-            initialState
+        console.log(this.state)
+        this.props.updateStudent(
+            this.state
         );
+        console.log("State: " + JSON.stringify(this.state));
+        alert("Current state: " + JSON.stringify(this.state));
     }
- 
-    const validate = (
+
+    handleBlur = (field) => (evt) => {
+        this.setState({
+            touched: {...this.state.touched, [field]: true}
+        });
+    }
+
+    validate = (
         sid,
         fullname,
         mobile,
         program,
-        father,
         mother,
+        father,
         fnum,
         email,
         address
@@ -77,55 +78,56 @@ function StudentUpdateForm(props) {
                 address: ''
             }
 
-            if(initialState.touched.sid && sid.length != 8)
+            if(this.state.touched.sid && sid.length !== 8)
                 errors.sid = 'Sid should be of 8 characters';
-            if(initialState.touched.fullname && fullname.length < 3 )
+            if(this.state.touched.fullname && fullname.length < 3 )
                 errors.fullname = 'Name should be greater than 3 characters';
-            if(initialState.touched.program && program.length < 3)
+            if(this.state.touched.program && program.length < 3)
                 errors.program = 'Program should be of minimum length of 3 characters';
-            if(initialState.touched.mother && mother.length > 30 || mother.length < 3)
+            if(this.state.touched.mother && (mother.length > 30 || mother.length < 3))
                 errors.mother = 'Name should not be greater than 30 characters and smaller than 3 characters';
-            if(initialState.touched.father && father.length > 30 || father.length < 3)
-                errors.mother = 'Name should not be greater than 30 characters and smaller than 3 characters';
-            if(initialState.touched.address && address.length < 5 || address.length > 50) 
+            if(this.state.touched.father && (father.length > 30 || father.length < 3))
+                errors.father = 'Name should not be greater than 30 characters and smaller than 3 characters';
+            if(this.state.touched.address && (address.length < 5 || address.length > 50)) 
                 errors.address = 'Address length should lie between 5 and 50 characters'
             const reg = /^\d{10}$/;
-            if(initialState.touched.mobile && !reg.test(mobile)) 
+            if(this.state.touched.mobile && !reg.test(mobile)) 
                 errors.mobile = 'Enter a valid Mobile Number';
-            if(initialState.touched.fnum && !reg.test(fnum)) 
+            if(this.state.touched.fnum && !reg.test(fnum)) 
                 errors.fnum = 'Enter a valid Mobile Number';
-            if(initialState.touched.email && email.split('').filter(x=>x === '@').length!=1)
+            if(this.state.touched.email && email.split('').filter(x=>x === '@').length!=1)
                 errors.email = 'Enter a valid email';
             
             return errors;
         }
 
-    const errors = validate(
-        initialState.sid,
-        initialState.fullname,
-        initialState.mobile,
-        initialState.program,
-        initialState.mother,
-        initialState.father,
-        initialState.fnum,
-        initialState.email,
-        initialState.address);
-    return (
-        <div>
+    render(){
+        const errors = this.validate(
+            this.state.sid,
+            this.state.fullname,
+            this.state.mobile,
+            this.state.program,
+            this.state.mother,
+            this.state.father,
+            this.state.fnum,
+            this.state.email,
+            this.state.address);
+        return (
+            <div>
             <div className="row">
                 <div className="col-12 container-fluid">
                     <h2 className="feature-heading ">Update Student Details</h2>
                     <hr className="feature-line" /> 
                 </div>  
             </div>
-            <Form className="myForm" onSubmit={handleSubmit}>
+            <Form className="myForm" onSubmit={this.handleSubmit}>
                     <Row form>
                       <Col md={6}>
                   <FormGroup>
                                 <Label htmlFor="sid">SID</Label>
-                                <Input type="text" id="sid" name="sid"
-                                    value={initialState.username}
-                                    onChange={handleInputChange} valid={errors.sid === ''} invalid={errors.sid !== ''} onBlur={handleBlur('sid')} />
+                                <Input required type="text" id="sid" name="sid"
+                                    value={this.state.sid}
+                                    onChange={this.handleInputChange} valid={errors.sid === ''} invalid={errors.sid !== ''} onBlur={this.handleBlur('sid')} />
                                     <FormFeedback>{errors.sid}</FormFeedback>
                             </FormGroup>
                             </Col>
@@ -134,8 +136,8 @@ function StudentUpdateForm(props) {
                     
                                 <Label htmlFor="fullname">Fullname</Label>
                                 <Input type="text" id="fullname" name="fullname"
-                                    value={initialState.fullname}
-                                    onChange={handleInputChange} valid={errors.fullname === ''} invalid={errors.fullname !== ''} onBlur={handleBlur('fullname')} />
+                                    value={this.state.fullname}
+                                    onChange={this.handleInputChange} valid={errors.fullname === ''} invalid={errors.fullname !== ''} onBlur={this.handleBlur('fullname')} />
                                 <FormFeedback>{errors.fullname}</FormFeedback>
                             </FormGroup>
                             </Col>
@@ -145,8 +147,8 @@ function StudentUpdateForm(props) {
                             <FormGroup>
                                 <Label htmlFor="mobile">Mobile No.</Label>
                                 <Input type="text" id="mobile" name="mobile"
-                                    value={initialState.mobile}
-                                    onChange={handleInputChange} valid={errors.mobile === ''} invalid={errors.mobile !== ''} onBlur={handleBlur('mobile')} />
+                                    value={this.state.mobile}
+                                    onChange={this.handleInputChange} valid={errors.mobile === ''} invalid={errors.mobile !== ''} onBlur={this.handleBlur('mobile')} />
                                 <FormFeedback>{errors.mobile}</FormFeedback>
                             </FormGroup>
                             </Col>
@@ -154,8 +156,8 @@ function StudentUpdateForm(props) {
                             <FormGroup>
                                 <Label htmlFor="program">Program</Label>
                                 <Input type="text" id="program" name="program"
-                                    value={initialState.program}
-                                    onChange={handleInputChange} valid={errors.sid === ''} invalid={errors.sid !== ''} onBlur={handleBlur('program')} />
+                                    value={this.state.program}
+                                    onChange={this.handleInputChange} valid={errors.program === ''} invalid={errors.program !== ''} onBlur={this.handleBlur('program')} />
                                 <FormFeedback>{errors.program}</FormFeedback>
                             </FormGroup>
                             </Col>
@@ -165,8 +167,8 @@ function StudentUpdateForm(props) {
                             <FormGroup>
                                 <Label htmlFor="father">Father Name</Label>
                                 <Input type="text" id="father" name="father"
-                                    value={initialState.father}
-                                    onChange={handleInputChange} valid={errors.father === ''} invalid={errors.father !== ''} onBlur={handleBlur('father')} />
+                                    value={this.state.father}
+                                    onChange={this.handleInputChange} valid={errors.father === ''} invalid={errors.father !== ''} onBlur={this.handleBlur('father')} />
                                 <FormFeedback>{errors.father}</FormFeedback>
                             </FormGroup>
                             </Col>
@@ -175,8 +177,8 @@ function StudentUpdateForm(props) {
                             <FormGroup>
                                 <Label htmlFor="fnum">Father Mobile</Label>
                                 <Input type="text" id="fnum" name="fnum"
-                                    value={initialState.fnum}
-                                    onChange={handleInputChange} valid={errors.fnum === ''} invalid={errors.fnum !== ''} onBlur={handleBlur('fnum')} />
+                                    value={this.state.fnum}
+                                    onChange={this.handleInputChange} valid={errors.fnum === ''} invalid={errors.fnum !== ''} onBlur={this.handleBlur('fnum')} />
                                 <FormFeedback>{errors.fnum}</FormFeedback>
                             </FormGroup>
                             </Col>
@@ -186,8 +188,8 @@ function StudentUpdateForm(props) {
                             <FormGroup>
                                 <Label htmlFor="mother">Mother Name</Label>
                                 <Input type="text" id="mother" name="mother"
-                                    value={initialState.mother}
-                                    onChange={handleInputChange} valid={errors.mother === ''} invalid={errors.mother !== ''} onBlur={handleBlur('mother')} />
+                                    value={this.state.mother}
+                                    onChange={this.handleInputChange} valid={errors.mother === ''} invalid={errors.mother !== ''} onBlur={this.handleBlur('mother')} />
                                 <FormFeedback>{errors.mother}</FormFeedback>
                             </FormGroup>
                             </Col>
@@ -195,8 +197,8 @@ function StudentUpdateForm(props) {
                             <FormGroup>
                                 <Label htmlFor="email">Email</Label>
                                 <Input type="text" id="email" name="email"
-                                    value={initialState.email}
-                                    onChange={handleInputChange} valid={errors.email === ''} invalid={errors.email !== ''} onBlur={handleBlur('email')} />
+                                    value={this.state.email}
+                                    onChange={this.handleInputChange} valid={errors.email === ''} invalid={errors.email !== ''} onBlur={this.handleBlur('email')} />
                                 <FormFeedback>{errors.email}</FormFeedback>
                             </FormGroup>
                                 </Col>
@@ -204,8 +206,8 @@ function StudentUpdateForm(props) {
                             <FormGroup>
                                 <Label htmlFor="address">P. Address</Label>
                                 <Input type="text" id="address" name="address"
-                                   value={initialState.address}
-                                   onChange={handleInputChange} valid={errors.address === ''} invalid={errors.address !== ''} onBlur={handleBlur('address')} />
+                                   value={this.state.address}
+                                   onChange={this.handleInputChange} valid={errors.address === ''} invalid={errors.address !== ''} onBlur={this.handleBlur('address')} />
                                 <FormFeedback>{errors.address}</FormFeedback>
                             </FormGroup>
                             <Button type="submit" color="primary">
@@ -213,7 +215,8 @@ function StudentUpdateForm(props) {
                             </Button> 
                   </Form>
         </div>
-    )
+        )
+    }
 }
 
-export default StudentUpdateForm
+export default StudentUpdateForm;
